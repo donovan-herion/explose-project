4acadd15a727a20035f862bed258ac9b14c69351
donovanherion <donovanherion@gmail.com>
2020-11-18 13:29:36 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontSize: {
+        giant: "10rem",
+        plus: "14rem",
+      },
+      fontFamily: {
+        helvetica: ['"Helvetica Neue"', "Arial"],
+      },
+      spacing: {
+        66: "20rem",
+        70: "22rem",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
